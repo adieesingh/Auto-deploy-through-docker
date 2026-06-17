e4b641e91e7ba59500cd199e5eba64d02f095630
unknown <1606adarsh@gmail.com>
2026-06-17 20:12:09 +0530

    Change:6 backend

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,6 +3,7 @@ export default async function Home(){
     const user = await prismaClient.user.findMany()
   return <div style={{display:"flex",height:"100px"}}>
     {JSON.stringify(user)}
+    <div>Helloo</div>
   </div>
 }
 
